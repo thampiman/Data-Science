f36d5fd2eb014e5b024d9f08e210dc3ffc206404
Ajay Thampi <ajay.thampi@gmail.com>
2014-10-08 20:19:06 +0100

    Cleaned up PIMRC data from 2002-2012 added

--- Communications-Research/cleanup_processed_ieee_data.py
+++ Communications-Research/cleanup_processed_ieee_data.py
@@ -7,7 +7,7 @@ def main():
     
     #input_files = ['iswcs','ew','wowmom','wcnc','vtc_spring','vtc_fall','icc',
     #               'globecom','pimrc','jsac','tvt','twc','letters']
-    input_files = ['globecom']
+    input_files = ['pimrc']
     
     for file in input_files:
         for yeari in yearsi:
