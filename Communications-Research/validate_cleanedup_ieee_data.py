ee7ffe15cfc084f43d4d71a75955b1570c6245fa
Ajay Thampi <ajay.thampi@gmail.com>
2014-10-12 15:22:28 +0100

    Validated PIMRC data from 2002-2012 added

--- Communications-Research/validate_cleanedup_ieee_data.py
+++ Communications-Research/validate_cleanedup_ieee_data.py
@@ -7,7 +7,7 @@ def main():
     
     #input_files = ['iswcs','ew','wowmom','wcnc','vtc_spring','vtc_fall','icc',
     #               'globecom','pimrc','jsac','tvt','twc','letters']
-    input_files = [('pimrc','conference')]
+    input_files = [('letters','letters')]
     
     for (file,type) in input_files:
         for yeari in yearsi:
