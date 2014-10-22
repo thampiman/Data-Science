44239fd0ab2d1e03734dc9c7f58045af41698a79
Ajay Thampi <ajay.thampi@gmail.com>
2014-10-22 18:57:35 +0100

    Consolidated tags data added grouped by year

--- Communications-Research/consolidate_validated_ieee_data.py
+++ Communications-Research/consolidate_validated_ieee_data.py
@@ -1,4 +1,6 @@
 import json
+import csv
+from os import remove
 
 def main():
     # main function
@@ -10,25 +12,40 @@ def main():
     types = ['conference','conference','conference','conference','conference',
              'conference','conference','conference','conference','journal',
              'journal','journal','journal']
-             
+    
+    cleanup_data(yearsi)         
     consolidate_ieee_data(yearsi,codes,types)
+    consolidate_tags_by_year(yearsi,codes)
+
+def cleanup_data(yearsi):
+    try:
+        ieee_file = 'final-data/ieee_data.json'
+        remove(ieee_file)
+    
+        for yeari in yearsi:
+            year = str(yeari)
+            tags_file = 'final-data/tags_' + year + '.csv'
+            remove(tags_file)
+    except OSError:
+        pass
 
 def consolidate_ieee_data(yearsi,codes,types):
     output_data = []
     output_file = 'final-data/ieee_data.json'
     
+    print '\n\nConsolidating IEEE Data'
     for index, code in enumerate(codes):
         type = types[index]
         for yeari in yearsi:
             year = str(yeari)
             input_file = 'validated-data/' + code + '_v_' + year + '.json'
-            consolidate_data(input_file,code,type,output_data)
+            add_data(input_file,code,type,output_data)
         
     print '\nConsolidating data to file: ' + output_file
     with open(output_file,'w') as f:
         json.dump(output_data,f)
 
-def consolidate_data(input_file,code,type,output_data):
+def add_data(input_file,code,type,output_data):
     data = json.loads(open(input_file).read())
     
     print '\nProcessing data from file: ' + input_file
@@ -46,5 +63,33 @@ def consolidate_data(input_file,code,type,output_data):
         
         output_data.append(paper)
 
+def consolidate_tags_by_year(yearsi,codes):
+    print '\n\nConsolidating Tags Data'
+    for yeari in yearsi:
+        year = str(yeari)
+        output_file = 'final-data/tags_' + year + '.csv'
+        for code in codes:
+            input_file = 'validated-data/' + code + '_v_' + year + '.json'
+            add_tags(input_file,output_file)
+
+def add_tags(input_file,output_file):
+    data = json.loads(open(input_file).read())
+    
+    print '\nProcessing data from file: ' + input_file
+    with open(output_file,'ab') as csvfile:
+        writer = csv.writer(csvfile,delimiter=',')
+        for paper in data:
+            authors = paper['authors']
+            tags = paper['tags']
+            citations = paper['citations']
+        
+            if len(authors) == 0 or len(tags) == 0:
+                continue
+        
+            tags = tags.split(',')
+            for tag in tags:
+                tag = tag.lower()
+                writer.writerow([tag,citations])
+        
 if __name__ == "__main__":
     main()
