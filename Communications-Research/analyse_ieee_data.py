73dec1334a1a4d82d64d29e66f639c316bd9178f
Ajay Thampi <ajay.thampi@gmail.com>
2014-10-22 18:20:34 +0100

    Better data consolidation and data loading for analysis

--- Communications-Research/analyse_ieee_data.py
+++ Communications-Research/analyse_ieee_data.py
@@ -8,46 +8,12 @@ def main():
     ieee_data = load_ieee_data()
     
     # Analyse the IEEE data
-    analyse_publications(ieee_data)
+    # analyse_tags(ieee_data)
     analyse_countries(ieee_data)
+    # analyse_publications(ieee_data)
     
 def load_ieee_data():
-    # Conferences
-    ew = pd.read_json('final-data/ew.json')
-    globecom = pd.read_json('final-data/globecom.json')
-    icc = pd.read_json('final-data/icc.json')
-    iswcs = pd.read_json('final-data/iswcs.json')
-    pimrc = pd.read_json('final-data/pimrc.json')
-    vtc_fall = pd.read_json('final-data/vtc_fall.json')
-    vtc_spring = pd.read_json('final-data/vtc_spring.json')
-    wcnc = pd.read_json('final-data/wcnc.json')
-    wowmom = pd.read_json('final-data/wowmom.json')
-    
-    # Journals
-    jsac = pd.read_json('final-data/jsac.json')
-    letters = pd.read_json('final-data/letters.json')
-    tvt = pd.read_json('final-data/tvt.json')
-    twc = pd.read_json('final-data/twc.json')
-    
-    # Add additional columns
-    add_code_type_columns(ew,'ew','conference')
-    add_code_type_columns(globecom,'globecom','conference')
-    add_code_type_columns(icc,'icc','conference')
-    add_code_type_columns(iswcs,'iswcs','conference')
-    add_code_type_columns(pimrc,'pimrc','conference')
-    add_code_type_columns(vtc_fall,'vtc_fall','conference')
-    add_code_type_columns(vtc_spring,'vtc_spring','conference')
-    add_code_type_columns(wcnc,'wcnc','conference')
-    add_code_type_columns(wowmom,'wowmom','conference')
-    add_code_type_columns(jsac,'jsac','journal')
-    add_code_type_columns(letters,'letters','journal')
-    add_code_type_columns(tvt,'tvt','journal')
-    add_code_type_columns(twc,'twc','journal')
-    
-    # Consolidate all data into one data frame
-    ieee_data = pd.concat([ew,globecom,icc,iswcs,pimrc,
-                           vtc_fall,vtc_spring,wcnc,wowmom,
-                           jsac,letters,tvt,twc])
+    ieee_data = pd.read_json('final-data/ieee_data.json')
     
     # Format Country and Tags strings
     country_format = lambda x: np.NaN if x == 'NA' else x.lower()
@@ -57,28 +23,9 @@ def load_ieee_data():
     
     return ieee_data
 
-def add_code_type_columns(df,code,type):
-    df['code'] = code
-    df['type'] = type
-
-def analyse_publications(ieee_data):
-    pubs_by_yr_code = ieee_data.groupby(['code','year'])['citations'].count().unstack()
-    citations_by_yr_code = ieee_data.groupby(['code','year'])['citations'].sum().unstack()
-    quality_by_yr_code = citations_by_yr_code / pubs_by_yr_code
-    
-    '''
-    print 'Number of Publications grouped by Code and Year'
-    print '-----------------------------------------------'
-    print pubs_by_yr_code
-    print '\n'
-    print 'Quality of Publications grouped by Code and Year'
-    print '------------------------------------------------'
-    print quality_by_yr_code
-    print '\n'
-    '''
-    
-    #pubs_by_yr_code.to_csv('analysed-data/publications_by_year_code.csv')
-    #quality_by_yr_code.to_csv('analysed-data/quality_of_publications_by_year_code.csv')
+def analyse_tags(ieee_data):
+    grouped = ieee_data.groupby(['year','tags'])
+    data_by_year = grouped.sum()
 
 def analyse_countries(ieee_data):
     grouped = ieee_data.groupby('country')
@@ -111,6 +58,23 @@ def analyse_countries(ieee_data):
     pubs.to_csv('analysed-data/pubs_by_country.csv')
     citations_filtered.to_csv('analysed-data/citations_by_country.csv')
     citations_per_pub.to_csv('analysed-data/citations_per_pub_by_country.csv')
+
+def analyse_publications(ieee_data):
+    pubs_by_yr_code = ieee_data.groupby(['code','year'])['citations'].count().unstack()
+    citations_by_yr_code = ieee_data.groupby(['code','year'])['citations'].sum().unstack()
+    quality_by_yr_code = citations_by_yr_code / pubs_by_yr_code
+
+    print 'Number of Publications grouped by Code and Year'
+    print '-----------------------------------------------'
+    print pubs_by_yr_code
+    print '\n'
+    print 'Quality of Publications grouped by Code and Year'
+    print '------------------------------------------------'
+    print quality_by_yr_code
+    print '\n'
+
+    #pubs_by_yr_code.to_csv('analysed-data/publications_by_year_code.csv')
+    #quality_by_yr_code.to_csv('analysed-data/quality_of_publications_by_year_code.csv')
 
 if __name__ == "__main__":
     main()
--- Communications-Research/consolidate_validated_ieee_data.py
+++ Communications-Research/consolidate_validated_ieee_data.py
@@ -4,26 +4,37 @@ def main():
     # main function
     yearsi = range(2002,2013)
     
-    input_files = ['iswcs','ew','wowmom','wcnc','vtc_spring','vtc_fall','icc',
-                   'globecom','pimrc','jsac','tvt','twc','letters']
+    codes = ['iswcs','ew','wowmom','wcnc','vtc_spring','vtc_fall','icc',
+             'globecom','pimrc','jsac','tvt','twc','letters']
+             
+    types = ['conference','conference','conference','conference','conference',
+             'conference','conference','conference','conference','journal',
+             'journal','journal','journal']
+             
+    consolidate_ieee_data(yearsi,codes,types)
+
+def consolidate_ieee_data(yearsi,codes,types):
+    output_data = []
+    output_file = 'final-data/ieee_data.json'
     
-    for file in input_files:
-        output_data = []
-        output_file = 'final-data/' + file + '.json'
+    for index, code in enumerate(codes):
+        type = types[index]
         for yeari in yearsi:
             year = str(yeari)
-            input_file = 'validated-data/' + file + '_v_' + year + '.json'
-            consolidate_data(input_file,output_file,output_data)
+            input_file = 'validated-data/' + code + '_v_' + year + '.json'
+            consolidate_data(input_file,code,type,output_data)
         
-        print '\nConsolidating data to file: ' + output_file
-        with open(output_file,'w') as f:
-            json.dump(output_data,f)
+    print '\nConsolidating data to file: ' + output_file
+    with open(output_file,'w') as f:
+        json.dump(output_data,f)
 
-def consolidate_data(input_file,output_file,output_data):
+def consolidate_data(input_file,code,type,output_data):
     data = json.loads(open(input_file).read())
     
     print '\nProcessing data from file: ' + input_file
     for paper in data:
+        paper['code'] = code
+        paper['type'] = type
         title = paper['title']
         authors = paper['authors']
         tags = paper['tags']
